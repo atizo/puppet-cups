5aac8b84d05991bf38d9e34b1012da7a481e45c0
Marcel Haerry <haerry@puzzle.ch>
2008-04-29 18:35:22 +0200

    added cups disabler

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,17 @@
 
 
 # modules_dir { "cups": }
-class cups {}
+class cups {
+    include cups::base 
+}
+
+class cups::base {
+    package{cups:
+        ensure => present,
+    }
+    service{cups:
+        ensure => running,
+        enable => true,
+        require => Package[cups],
+    }
+}
